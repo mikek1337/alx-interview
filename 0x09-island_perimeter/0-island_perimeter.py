d1b0f449ef009f644d4d05a5a9a3c399234d3c1f
mikek1337 <mikekegnlyew@gmail.com>
2023-08-31 08:31:53 +0300

    improved

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,10 +6,12 @@ def island_perimeter(grid):
     """calculate the parameter of island"""
     parameter = 0
     for i in range(len(grid)):
-        print(grid[i])
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
                 parameter += 1
-                if (grid[i][j+1] == 1 and grid[i-1][j] == 1):
-                    parameter += 1
+                if (grid[i][j] != len(grid[i])):
+                    if (grid[i][j+1] == 1 and grid[i-1][j] == 1):
+                        parameter += 1
+    if (len(grid) == 1):
+        parameter = parameter*2
     return 2*parameter
